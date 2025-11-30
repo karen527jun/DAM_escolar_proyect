f917b86ef696f8e35316f5e2754300cf904ed69e
Karen Martinez <karenmartinezriv527@gmail.com>
2025-11-30 08:20:37 -0600

    FIX(notaas backend) se colocó un comentario

--- src/services/network.services.js
+++ src/services/network.services.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
-
-const BASE_URL = "https://expressbackescuela-production.up.railway.app/api";
+// COMO EL BACKEND SE ENCUENTRA EN LA NUBE ACÁ SE LLAMA A LA API, SI NO ESTA EN LA NUBE, SUBIR EL BACCKEND MANUALMENTE Y CAMBIARLO
+const BASE_URL =
+  "https://expressbackescuela-production.up.railway.app/api" ||
+  "http://localhost:3000/api";
 
 const config = {
-    baseURL: BASE_URL,
-    headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        Authorization: ``,
-    },
-}
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: ``,
+  },
+};
 
 const instance = axios.create(config);
 
